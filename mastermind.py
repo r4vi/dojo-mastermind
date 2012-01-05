78569a8d6670a27452a442401483b877cd4a9e36
r4vi <kotecha.ravi@gmail.com>
2012-01-05 20:41:19 +0000

    second commit

--- mastermind.py
+++ mastermind.py
@@ -3,16 +3,35 @@ COLOURS = ['A', 'B', 'C', 'D']
 WIDTH = 4
 
 class Mastermind(object):
-	def create_problem(self):
-		random.shuffle(COLOURS)
+	def __init__(self):
+		self.problem = [random.choice(COLOURS) for n in range(WIDTH)]
+		self.score = [0]*WIDTH
+
 	def check_answer(self):
+		for i,x in enumerate(self.guess):
+			if x == self.problem[i]:
+				self.score[i] = 2
+			elif x in self.problem:
+				self.score[i] = 1
+			else:
+				self.score[i] = 0
+	def __repr__(self):
 		pass
-	def print_board(self):
-		pass
+
 	def get_input(self):
-		pass
+		print "Enter your guess:"
+		self.guess = raw_input().upper()
+
 	def run(self):
-		pass	
+		while True:
+			self.get_input()
+			print self.problem
+			self.check_answer()
+			print self.score
+			if self.score == [2]*WIDTH:
+				print "you are a mastermind"
+				break
+
 
 if __name__ == '__main__':
 	game = Mastermind()
